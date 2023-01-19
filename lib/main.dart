4a241c0c26412e29460de6b4d0effef6c22e222d
Devika Sunil <109059690+Devikasunil@users.noreply.github.com>
2023-01-19 17:47:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() {
   runApp(FilterCamApp());
 }
 
-class CameraApp extends StatelessWidget {
-  CameraApp({super.key});
+class FilterCamApp extends StatelessWidget {
+  FilterCamApp({super.key});
 
   @override
   Widget build(BuildContext context) {
